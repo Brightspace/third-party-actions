0d16992e05ea51de938f9701fe786be8e882ddf6
Cheick Abba Cisse <cheick95@hotmail.com>
2020-07-28 14:12:05 -0400

    Add input for branch name, description and options for push (#4)
    * Add input for branch name, description and options for push
    
    * Add missing inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ GIT_USER_NAME=${1}
 GIT_USER_EMAIL=${2}
 PACKAGE_MANAGER=${3}
 VERSION_TYPE=${4}
+BRANCH=${5}
+PR_DESCRIPTION=${6}
+PUSH_OPTIONS=${7}
 
 if [ "${PACKAGE_MANAGER}" == 'npm' ]; then
   npm version --no-git-tag-version ${VERSION_TYPE}
@@ -12,14 +15,23 @@ elif [ "${PACKAGE_MANAGER}" == 'yarn' ]; then
   yarn version --no-git-tag-version "--${VERSION_TYPE}"
 fi
 
-DESCRIPTION="chore: bump ${VERSION_TYPE} version ($(date -I))"
-PR_BRANCH=chore/version-$(date +%s)
+if [ -n "${PR_DESCRIPTION}" ]; then
+  DESCRIPTION=${PR_DESCRIPTION}
+else 
+  DESCRIPTION="chore: bump ${VERSION_TYPE} version ($(date -I))"
+fi
+
+if [ -n "${BRANCH}" ]; then
+  PR_BRANCH=${BRANCH}
+else 
+  PR_BRANCH=chore/version-$(date +%s)
+fi
 
 git config user.name ${GIT_USER_NAME}
 git config user.email ${GIT_USER_EMAIL}
 git checkout -b ${PR_BRANCH}
 git commit -am "${DESCRIPTION}"
-git push origin ${PR_BRANCH}
+git push ${PUSH_OPTIONS} origin ${PR_BRANCH}
 
 curl -fsSL https://github.com/github/hub/raw/master/script/get | bash -s 2.14.1
 bin/hub pull-request -m "${DESCRIPTION}"
